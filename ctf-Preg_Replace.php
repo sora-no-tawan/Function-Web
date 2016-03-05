8adba92dcbe96b943577fa46f94e3c43bd8a9a5d
Tawan Naultang <setbanned@gmail.com>
2016-03-05 11:15:03 +0700

    Update and rename ctf-Preg_Replace to ctf-Preg_Replace.php

--- ctf-Preg_Replace.php
+++ ctf-Preg_Replace.php
@@ -4,4 +4,4 @@ if(isset($_POST['search']) && isset($_POST['replace']) && isset($_POST['content'
 	$new = preg_replace($_POST['search'], $_POST['replace'], $_POST['content']);
 	echo htmlentities($new);
 }
-?>
+
